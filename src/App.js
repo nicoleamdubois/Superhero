a9b2b8790a402eda8da88e9388d16b6aafa47f53
Nicole <nicoleamdubois@hotmail.com>
2022-06-01 09:25:47 -0600

    Added superheros to App.js

--- src/App.js
+++ src/App.js
@@ -2,42 +2,23 @@ import { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
-  const [data, setData] = useState();
-  const [count, setCount] = useState(0);
-  const [count2, setCount2] = useState(0);
-  const [countDown, setCountDown] = useState(3);
-
-  useEffect(() => {
-    console.log(`COUNTDOWN: ${countDown}`);
-    if (countDown > 0) {
-      getTimeFromServer();
-    }
-  }, [countDown]);
-
-
-  // const getTimeFromServer = async () => {
-  //   console.log('clicked!');
-  //   try {
-  //     console.log('calling fetch()!');
-  //     let response = await fetch('/slow');
-  //     let vals = await response.json();
-  //     setCountDown(curVal => curVal - 1);
-  //     return setData(vals.currentTime);
-  //   } catch (ex) {
-  //     console.log(ex);
-  //   }
-  // };
-
+ 
   return (
     <div className='App'>
       <header className='App-header'>
         <h1>Superheroes</h1>
        </header>
-      <div>
+        <div>
+        <h2>Ironman</h2>
         <p>Alter Ego</p>
         <p>Tony Stark</p>
-      </div>
-
+        <p>Superpowers:</p>
+        <ul>
+          <li>Superhuman strength</li>
+          <li>Invulnerabiity</li>
+        </ul>
+        </div>
+</div>
   );
 }
 
